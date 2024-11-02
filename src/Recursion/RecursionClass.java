3c96977d9c9b8700567571fbf6fc2a6ab2976403
ankhan <ankhan@nisum.com>
2024-11-02 20:19:11 +0500

    Sum of natural Numbers

--- src/Recursion/RecursionClass.java
+++ src/Recursion/RecursionClass.java
@@ -1,6 +1,11 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class RecursionClass {
+
     public void printValue(Long number, Long limit) {
 
         if (number > limit) {
@@ -11,5 +16,18 @@ public class RecursionClass {
 
         number++;
         printValue(number, limit);
+    }
+
+    public void sumOfNaturalNumbers(int sum, int pointer, int limit) {
+
+        sum = sum + pointer;
+
+        if (pointer == limit) {
+            System.out.println(sum);
+            return;
+        }
+
+        pointer++;
+        sumOfNaturalNumbers(sum, pointer, limit);
     }
 }
--- src/Recursion/main.java
+++ src/Recursion/main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
 
         RecursionClass job = new RecursionClass();
-        job.printValue(1L, 100L);
-
+//        job.printValue(1L, 100L);
+        job.sumOfNaturalNumbers(0, 0, 10);
     }
 }
