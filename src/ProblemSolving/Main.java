ec0a281330ff1a1d45a49c11504af300f9a6ea2d
ankhan <ankhan@nisum.com>
2024-11-05 17:25:28 +0500

    Some code refactored

--- src/ProblemSolving/Main.java
+++ src/ProblemSolving/Main.java
@@ -8,7 +8,6 @@ public class Main {
         int[] arr = new int[] {1,2,3,4,5,6,7,8,9,10};
         int target = 15;
 
-
         ProblemSolving problemSolving = new ProblemSolving();
         System.out.println(problemSolving.subArraySum(arr, target));
     }
