126d8bbdaafb28f69de047573c3b2047d3a14091
ankhan <ankhan@nisum.com>
2024-12-24 18:05:52 +0500

    Input implemented

--- src/Multithreading/Task_2/main.java
+++ src/Multithreading/Task_2/main.java
@@ -1,10 +1,17 @@
 package Multithreading.Task_2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        String string = "Bobby";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Please provide String to e reversed:");
+        String string = reader.readLine();
 
         ReverseStr reverseStr = new ReverseStr();
         System.out.println(reverseStr.reverseStr(string, "", string.length()-1));
